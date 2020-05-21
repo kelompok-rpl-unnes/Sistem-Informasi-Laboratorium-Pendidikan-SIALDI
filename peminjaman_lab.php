ff512fdc515a3b253ed3bff52397c89c7ca467ac
Chyntiaekaputri <64616697+Chyntiaekaputri@users.noreply.github.com>
2020-05-21 23:29:37 +0700

    Update peminjaman_lab.php

--- peminjaman_lab.php
+++ peminjaman_lab.php
@@ -1,35 +1,107 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_peminjaman extends CI_Model {
+class peminjaman_lab extends CI_Controller {
 
-    public function getData($value='')
-    {
-        $this->db->from('peminjaman pj');
-        $this->db->order_by('pj.id', 'desc');
-        return $this->db->get();
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->fungsi->restrict();
+		$this->load->model('peminjaman/m_peminjaman_lab');
+	}
 
-    public function insertData($data='')
-    {
-        
-        $this->db->insert('peminjaman',$data);
-       
-    }
+	public function index()
+	{
+		$this->fungsi->check_previleges('peminjaman_lab');
+		$data['peminjaman_lab'] = $this->m_tipe_lab->getData();
+		$this->load->view('peminjaman/peminjaman_lab/v_peminjaman_lab_list',$data);
+	}
+	
+	public function form($param='')
+	{
+		$content   = "<div id='divsubcontent'></div>";
+		$header    = "PEMINJAMAN LABORATORIUM";
+		$subheader = "peminjaman_lab";
+		$buttons[] = button('jQuery.facebox.close()','Tutup','btn btn-default','data-dismiss="modal"');
+		echo $this->fungsi->parse_modal($header,$subheader,$content,$buttons,"");
+		if($param=='base'){
+			$this->fungsi->run_js('load_silent("peminjaman/peminjaman_lab/show_addForm/","#divsubcontent")');	
+		}else{
+			$base_kom=$this->uri->segment(5);
+			$this->fungsi->run_js('load_silent("peminjaman/peminjaman_lab/show_editForm/'.$base_kom.'","#divsubcontent")');	
+		}
+	}
 
-    public function updateData($data='')
-    {
-         $this->db->where('id',$data['id']);
-            $this->db->update('peminjaman',$data);
-    }
+	public function show_addForm()
+	{
+		$this->fungsi->check_previleges('peminjaman_lab');
+		$this->load->library('form_validation');
+		$config = array(
+				array(
+					'field'	=> 'nama_peminjam',
+					'label' => 'nama_peminjam',
+					'rules' => 'required'
+				)
+			);
+		$this->form_validation->set_rules($config);
+		$this->form_validation->set_error_delimiters('<span class="error-span">', '</span>');
 
-    public function deleteData($id='')
-    {
-        $this->db->where('id', $id);
-        $this->db->delete('peminjaman');
-    }
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['status']='';
+			$this->load->view('peminjaman/peminjaman_lab/v_peminjaman_lab_add',$data);
+		}
+		else
+		{
+			$datapost = get_post_data(array('id_pinjam','nama_peminjam','tgl_pinjam','tgl_selesai','status_pinjam'));
+			$this->m_tipe_lab->insertData($datapost);
+			$this->fungsi->run_js('load_silent("peminjaman/peminjaman_lab","#content")');
+			$this->fungsi->message_box("Data Peminjaman Laboratorium sukses disimpan...","success");
+			$this->fungsi->catat($datapost,"Menambah eminjaman_lab dengan data sbb:",true);
+		}
+	}
 
+	public function show_editForm($id='')
+	{
+		$this->fungsi->check_previleges('tipe_lab');
+		$this->load->library('form_validation');
+		$config = array(
+				array(
+					'field'	=> 'id_pinjam',
+					'label' => 'wes mbarke',
+					'rules' => ''
+				),
+				array(
+					'field'	=> 'nama_peminjam',
+					'label' => 'nama_peminjam',
+					'rules' => 'required'
+				)
+			);
+		$this->form_validation->set_rules($config);
+		$this->form_validation->set_error_delimiters('<span class="error-span">', '</span>');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['edit'] = $this->db->get_where('tipe_lab',array('id'=>$id));
+			$data['status']='';
+			$this->load->view('peminjaman/peminjaman_lab/v_peminjaman_lab_edit',$data);
+		}
+		else
+		{
+			$datapost = get_post_data(array('id_pinjam','nama_peminjam','tgl_pinjam','tgl_selesai','status_pinjam'));
+			$this->m_tipe_lab->updateData($datapost);
+			$this->fungsi->run_js('load_silent("peminjaman/peminjaman_lab","#content")');
+			$this->fungsi->message_box("Data Peminjaman Laboratorium sukses diperbarui...","success");
+			$this->fungsi->catat($datapost,"Mengedit peminjaman_lab dengan data sbb:",true);
+		}
+	}
+	public function delete()
+	{
+		$id = $this->uri->segment(4);
+		$this->m_tipe_lab->deleteData($id);
+		redirect('admin');
+	}
 }
 
-/* End of file m_peminjaman.php */
-/* Location: ./application/models/master/m_peminjaman.php */
+/* End of file tipe_lab.php */
+/* Location: ./application/controllers/master/tipe_lab.php */
