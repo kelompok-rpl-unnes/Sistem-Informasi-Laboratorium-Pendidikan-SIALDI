b1adc245b92e46fdc56dad6885ccac8922ffd7e7
Fikry-Muhanna <64646301+Fikry-Muhanna@users.noreply.github.com>
2020-05-28 20:28:17 +0700

    Update v_kategori_alat_bahan_edit.php

--- v_kategori_alat_bahan_edit.php
+++ v_kategori_alat_bahan_edit.php
@@ -6,11 +6,17 @@
 <div class="box-body big">
     <?php echo form_open('',array('name'=>'faddmenugrup','class'=>'form-horizontal','role'=>'form'));?>
         
-   
+        <div class="form-group">
+            <label class="col-sm-4 control-label">id alat bahan</label>
+            <div class="col-sm-8">
+            <?php echo form_hidden('id',$row->id); ?>
+            <?php echo form_input(array('name'=>'id_alat_bahan','value'=>$row->id_alat_bahan,'class'=>'form-control'));?>
+            <?php echo form_error('id_alat_bahan');?>
+            </div>
+        </div>
         <div class="form-group">
             <label class="col-sm-4 control-label">Nama Alat</label>
             <div class="col-sm-8">
-            <?php echo form_hidden('id',$row->id); ?>
             <?php echo form_input(array('name'=>'nama_alat','value'=>$row->nama_alat,'class'=>'form-control'));?>
             <?php echo form_error('nama_alat');?>
             </div>
@@ -18,7 +24,6 @@
         <div class="form-group">
             <label class="col-sm-4 control-label">Nama Bahan</label>
             <div class="col-sm-8">
-            <?php echo form_hidden('id',$row->id); ?>
             <?php echo form_input(array('name'=>'nama_bahan','value'=>$row->nama_bahan,'class'=>'form-control'));?>
             <?php echo form_error('nama_bahan');?>
             </div>
@@ -34,7 +39,7 @@
             <label class="col-sm-4 control-label">Simpan</label>
             <div class="col-sm-8 tutup">
             <?php
-            echo button('send_form(document.faddmenugrup,"master/tipe_lab/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
+            echo button('send_form(document.faddmenugrup,"master/kategori_alat_bahan/show_editForm/","#divsubcontent")','Simpan','btn btn-success')." ";
             ?>
             </div>
         </div>
